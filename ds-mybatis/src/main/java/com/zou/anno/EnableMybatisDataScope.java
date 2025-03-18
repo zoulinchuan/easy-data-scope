102a26a07bbab09ba98b191f59fac1ca0bec370e
zoulinchuan <zoulinchuan2004@163.com>
2025-03-18 12:11:45 +0800

    anno

---
+++ ds-mybatis/src/main/java/com/zou/anno/EnableMybatisDataScope.java
@@ -0,0 +1,22 @@
+package com.zou.anno;
+
+import com.zou.MyBatisDataScopeConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author zoulinchuan
+ * @Date 2025年03月15日 星期六 22:51
+ * @description: 开启mybatis数据权限
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Configuration
+@Import(MyBatisDataScopeConfiguration.class)
+public @interface EnableMybatisDataScope {
+}
